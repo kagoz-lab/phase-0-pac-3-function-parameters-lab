8b3d72c966d9af42079f0d221089399f7fde6af1
kagoz-lab <kagokevin33@gmail.com>
2024-11-23 00:11:35 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+function introductionWithLanguageOptional(name ,language = "Javascript")
+{
+    console.log(`hello my name is ${name} and I am learning to program in ${language}`);
+    return;
+
+}
+introductionWithLanguageOptional("kevin");
